6fe01c3e2ffef12f5dfeaf5184380fd54926de7a
Aidan Minor <aidanminor6@gmail.com>
2026-03-17 09:12:03 -0400

    keys

---
+++ practice/keys.js
@@ -0,0 +1,29 @@
+
+const canvas = document.getElementById("canvas");
+				const ctx = canvas.getContext("2d");
+
+let x = 0;
+let vx = 6;
+let y = 0;
+let vy = 7;
+let gravity = 1;
+
+function move(){
+	ctx.clearRect(0,0,800,800);
+	if ( x > 750 || x < 0){
+		vx = vx * -1;
+	}
+	if ( y > 750 || y < 0){
+		vy = vy * -1;
+	ctx.fillRect(x,y,50,50);
+	requestAnimationFrame(move)
+}
+move();
+
+function handleKeyDown(e){
+	console.log("KEY EVENT");
+	if(e.key == "ArrowDown"){
+		y = y + vy;
+}
+
+document.addEventListener('keydown',handleKeyDown);
